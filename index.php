542522cae3a36c5908e1cc0f9d1f11f3412aa793
Adriano A Costa <sdcomputadores@gmail.com>
2017-03-09 14:16:41 -0300

    update index

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <?php
 
-$url = "http://oreilly.com";
+//$result = file_get_contents('http://requestb.in/sikfggsi');
+//echo $result;
+
+$url = "http://requestb.in/sikfggsi";
+$data = array("name" => "Lorna", "email" => "lorna@example.com");
 $ch = curl_init($url);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json')
+);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $result = curl_exec($ch);
-curl_close($ch);
 print_r($result);
